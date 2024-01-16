f12c362411019d35fcd8853696e7289546f91433
AC11DB <cadizangelicamael@gmail.com>
2024-01-16 10:18:32 -0700

    fixed log consoles

--- index.js
+++ index.js
@@ -3,4 +3,15 @@ const lastName = "Cadiz";
 let isActive = true;
 const userId = 2123;
 
-console.log('User ${userId}: ${firstName}: ${lastName}: ${isActive}. Zaboomafoo');
+//single log at a time
+console.log(firstName);
+console.log(lastName);
+
+//log multiple values
+console.log(userId, isActive);
+
+//log with mixed values
+console.log("User: " + firstName + " is " + userId);
+
+//template literals
+console.log(`User ${userId} ${firstName} ${lastName} ${isActive}. lllllll`);

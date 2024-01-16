47ac046a19460139d05e27179164e5c3f10d174c
AC11DB <cadizangelicamael@gmail.com>
2024-01-16 10:41:14 -0700

    adding complex data type

---
+++ complexDataTypes.js
@@ -0,0 +1,40 @@
+const userOne = {
+  userId: 1234,
+  firstName: "Angel",
+  lastName: "Cadiz",
+  isActive: true,
+};
+const userTwo = {
+  userId: 1235,
+  firstName: "Copernicus",
+  lastName: "Sees the Stars",
+  isActive: false,
+};
+const userThree = {
+  userId: 1236,
+  firstName: "Sally",
+  lastName: "Fields",
+  isActive: true,
+};
+
+//const user = [userOne, userTwo, userThree];
+const user = [
+  {
+  userId: 1234,
+  firstName: "Angel",
+  lastName: "Cadiz",
+  isActive: true,
+  },
+  {
+    userId: 1235,
+    firstName: "Copernicus",
+    lastName: "Sees the Stars",
+    isActive: false,
+  },
+  {
+  userId: 1236,
+  firstName: "Sally",
+  lastName: "Fields",
+  isActive: true,
+  }
+];

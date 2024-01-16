bd963a3f81367817f64027f63818855361fff5e9
AC11DB <cadizangelicamael@gmail.com>
2024-01-16 12:53:52 -0700

    functions

--- functions.js
+++ functions.js
@@ -1,28 +1,41 @@
-[
-const userGroup = {
-  userId: 1234,
-  firstName: "Angel",
-  lastName: "Cadiz",
-  isActive: true,
-};
-const userTwo = {
-  userId: 1235,
-  firstName: "Copernicus",
-  lastName: "Sees the Stars",
-  isActive: false,
-};
-const userThree = {
-  userId: 1236,
-  firstName: "Sally",
-  lastName: "Fields",
-  isActive: true,
-},
+
+  const userGroup = [
+    userId: 1234,
+    firstName: "Angel",
+    lastName: "Cadiz",
+    isActive: true,
+  },
+   const userTwo = {
+    userId: 1235,
+    firstName: "Copernicus",
+    lastName: "Sees the Stars",
+    isActive: false,
+  };
+   const userThree = {
+    userId: 1236,
+    firstName: "Sally",
+    lastName: "Fields",
+    isActive: true,
+  },
 ];
 
 //funciton declaration
-function printUser() {
-  console.log(userGroup[0]);
+function printUser(user) {
+  console.log(user);
 }
 
 //function invocation
-printUser();
+printUser(userGroup[0]);
+
+for (const user in userGroup) {
+  printUser(user);
+}
+
+function isActiveUser(user) {
+  if(user.status === true) {
+    console.log(true);
+    return true;
+  } else {
+    return false;
+  }
+}

73a4936e1d5ac9ce4a4a1865036ab6ff384dcb2c
AC11DB <cadizangelicamael@gmail.com>
2024-01-16 11:33:15 -0700

    added code into functions

---
+++ functions.js
@@ -0,0 +1,28 @@
+[
+const userGroup = {
+  userId: 1234,
+  firstName: "Angel",
+  lastName: "Cadiz",
+  isActive: true,
+};
+const userTwo = {
+  userId: 1235,
+  firstName: "Copernicus",
+  lastName: "Sees the Stars",
+  isActive: false,
+};
+const userThree = {
+  userId: 1236,
+  firstName: "Sally",
+  lastName: "Fields",
+  isActive: true,
+},
+];
+
+//funciton declaration
+function printUser() {
+  console.log(userGroup[0]);
+}
+
+//function invocation
+printUser();
